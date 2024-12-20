b6a951f42d1805b9a03f4738e2ff9e95577fbe8a
Leonidus786 <abhi.upadh@gmail.com>
2024-12-20 12:35:59 +0530

    First commit

---
+++ 1-Python Basics/test.py
@@ -0,0 +1,29 @@
+# This is a single line comment
+
+
+'''
+this is a exmaple of multiline comments
+'''
+'''
+Welcome to the
+python course
+
+
+'''
+
+
+'''
+The big brown fox 
+Jumps Over
+the lazy dog.
+'''
+
+"""
+this is 
+also a multiline
+comment.
+"""
+
+# 
+''''''
+""""""
